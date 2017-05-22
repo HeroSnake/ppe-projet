14611b169d264c9f697b9890e721b255963d0194
syx.florent <syx.florent@R20909>
2017-05-22 08:14:05 +0200

    Ajoutez des fichiers projet.

---
+++ WebAPPE/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPPE.MvcApplication" Language="C#" %>
---
+++ WebAPPE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAPPE.Startup))]
+namespace WebAPPE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
